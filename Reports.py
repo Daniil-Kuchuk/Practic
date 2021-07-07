1cffc97db8f2567a49ec8d06578eded8c5c9bc9a
Daniil-Kuchuk <dan982.k@gmail.com>
2021-07-07 17:30:26 +0300

    функция create_slide() создает слайды и добавляет картинки

--- Reports.py
+++ Reports.py
@@ -3,7 +3,7 @@ from PIL import Image
 import os.path
 import sys
 
-from pptx.util import Inches
+from pptx.util import Inches, Pt
 
 
 class Reports:
@@ -11,8 +11,6 @@ class Reports:
         self.__prefix = {}
         self.__path = path if os.path.exists(path) else None
         self.__walk = [item for item in os.walk(path)]
-        self.__numbers_row = len(self.__walk[0][1])
-        self.__numbers_columns = len(self.__walk[1][1])
         self.__numbers_slide = 0
         self.__images = {}
 
@@ -27,9 +25,25 @@ class Reports:
         self.__template()
 
         prs = Presentation()
-        slide = prs.slides.add_slide(prs.slide_layouts[5])
-        s = slide.shapes.add_picture('D:\\test\\сетка1\\q1\\Pn_field_x=2.png', Inches(1.0), Inches(0.5))
-        prs.save('test.pptx')
+        numbers_row = len(os.listdir(self.__path))
+        numbers_columns = len(os.listdir(os.chdir(self.__path + '\\' + os.listdir(self.__path)[0])))
+
+        for key, value in self.__prefix.items():
+            slide = prs.slides.add_slide(prs.slide_layouts[5])
+            slide.shapes.title.text = f'{value}'
+            for grid in range(numbers_row):
+                txBox = slide.shapes.add_textbox(Pt(30.0), Pt(25.5), Pt(23.0), Pt(22.5))
+                txBox.text_frame._bodyPr.attrib.update({'vert': 'vert'})
+                tf = txBox.text_frame
+                p = tf.add_paragraph()
+                p.text = f'сетка {grid+1}'
+                for col in range(numbers_columns):
+                    q_txt_box = slide.shapes.add_textbox(Pt(12.0), Pt(10.5), Pt(3.0), Pt(2.5))
+                    tf = q_txt_box.text_frame.add_paragraph()
+                    tf.text = f'q{col+1}'
+                    slide.shapes.add_picture(self.__images[f'{key}_grid{grid+1}_q{col+1}'], Inches(5.0), Inches(6.5))
+
+        prs.save('C:\\Users\\kuchu\\PycharmProjects\\Practic\\test.pptx')
 
     def add_prefix(self, prefix):
         for (key, value) in prefix.items():
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from Reports import Reports
 def main():
     p = Reports(path='D:\\test')
     # prefixes = input('Введите префиксы: ')
-    prefixes = 'Pn - давление, V - скорость, Gh - обозначение'
+    prefixes = 'Pn - давление, V - скорость'
     new_prefix = [prefix.split('-') for prefix in prefixes.split(',')]
     pref = {item[0].strip(): item[1].strip() for item in new_prefix}
 
