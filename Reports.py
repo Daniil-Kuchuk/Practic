ebc728db5c463bfa9d732d13ebc49d163ebde143
Daniil-Kuchuk <dan982.k@gmail.com>
2021-07-07 03:01:50 +0300

    обновленна функция __template()

--- Reports.py
+++ Reports.py
@@ -11,6 +11,7 @@ class Reports:
         self.__numbers_row = len(self.__walk[0][1])
         self.__numbers_columns = len(self.__walk[1][1])
         self.__numbers_slide = 0
+        self.__images = {}
 
     def __stop_program(self):
         if self.__path is None:
@@ -22,26 +23,20 @@ class Reports:
 
         self.__template()
 
+        prs = Presentation()
+        slide_layout = prs.slide_layouts[1]
+        slides = prs.slides.add_slide(slide_layout)
+
     def add_prefix(self, prefix):
         for (key, value) in prefix.items():
             self.__prefix[key] = value
         self.__numbers_slide = len(self.__prefix)
 
     def __template(self):
-        print(self.__walk)
-        slide = {}
-        for prefix in self.__walk[2][2]:
-            prefix = prefix.split('_')[0]
-            slide[self.__prefix[prefix]] = {}
+        for i, grid in enumerate(os.listdir(self.__path)):
+            for q in os.listdir(f'{self.__path}\\{grid}'):
+                for img in os.listdir(f'{self.__path}\\{grid}\\{q}'):
+                    self.__images[f'{img.split("_")[0]}_grid{i+1}_{q}'] = f'{self.__path}\\{grid}\\{q}\\{img}'
 
-        for grid in slide.values():
-            for a in self.__walk[0][1]:
-                grid[a] = {}
-
-        for item in slide.keys():
-            for grid in os.listdir(self.__path):
-                for q in os.listdir(self.__path + grid):
-                    pass
-
-
-        print(slide)
+        for item, val in self.__images.items():
+            print(f'{item}: {val}')
